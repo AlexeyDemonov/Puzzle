831f65e64c97b366b0b280f777a822778f4a909c
AlexeyDemonov <alexei.demonov.341@gmail.com>
2020-02-26 21:32:22 +0500

    Add project files.

---
+++ Puzzle/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Puzzle
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Image swap;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_DragImage(object sender, MouseButtonEventArgs e)
+        {
+            Image image = (Image)e.Source;
+            swap = image;
+            DataObject data = new DataObject(typeof(ImageSource), image.Source);
+            DragDrop.DoDragDrop(image, data, DragDropEffects.Move);
+        }
+
+        private void Handle_DropImage(object sender, DragEventArgs e)
+        {
+            ImageSource image = (ImageSource)e.Data.GetData(typeof(ImageSource));
+            Image dropTarget = (Image)sender;
+            swap.Source = dropTarget.Source;
+            dropTarget.Source = image;
+        }
+    }
+}
